99ceea2983bc2c5c17fa511c2ac8c539b097fe41
zeus <zeus@tissuematter.com>
2017-03-16 01:14:14 -0700

    on hosted box

---
+++ gi.sh
@@ -0,0 +1,8 @@
+git config --global user.name 'zeus'
+git config --global user.email 'zeus@tissuematter.com'
+git init
+git add .
+git commit -m "sh git setup"
+git remote add origin https://github.com/tissuematter/zeus.git
+git push -u origin master
+
